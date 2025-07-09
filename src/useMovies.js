2cb4c8ace9bbdfa4f0624631effc961d4a915241
Gafar Gbolahan <ksley4040@gmail.com>
2025-07-09 10:11:08 +0100

    Fix: use OMDB env variable in useMovies hook

--- src/useMovies.js
+++ src/useMovies.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-const KEY = "24e5dfa1";
+const KEY = process.env.REACT_APP_OMDB_API_KEY;
+
 export function useMovies(query) {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
